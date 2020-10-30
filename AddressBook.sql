d25cc8d89486ac8b21400ab647c0ea3e99f592e8
batista12 <50904472+batista12@users.noreply.github.com>
2020-10-29 18:55:50 -0700

    [Manasi]UC3 is refactored to add the table AddressBook

--- AddressBook.sql
+++ AddressBook.sql
@@ -4,11 +4,11 @@ use addressbook_service;
 #UC2
 create table addressbook
 (
-   first_name    varchar(150) not null,
-   last_name     varchar(150) not null,
-   address       varchar(400), 
-   city	   	 varchar(20),
-   state	 varchar(20),
+   first_name    varchar(100) not null,
+   last_name     varchar(100) not null,
+   address       varchar(500), 
+   city	   	 varchar(50),
+   state	 varchar(50),
    zip		 int unsigned,
    phone_number	 int unsigned,
    email_id    	 varchar(100) not null,
